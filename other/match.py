450303df6288060d97deb3c153e7a42e3bdb97f3
JJ Merelo <jjmerelo@gmail.com>
2017-12-19 17:49:25 +0100

    Improves code

--- other/match.py
+++ other/match.py
@@ -9,6 +9,6 @@ pp = pprint.PrettyPrinter(indent=4)
 match = re.findall("\*(\w+)\*", sys.argv[1])
 pp.pprint( match )
 
-match = re.match("\*(\w+)\*", sys.argv[1])
+match = re.search("\*(\w+)\*", sys.argv[1])
 pp.pprint( match )
 print( match.group(1) )
