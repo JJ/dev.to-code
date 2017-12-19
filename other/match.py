e2e9e1bb6623c645e2fdaa2e26be320a2111c97c
JJ Merelo <jjmerelo@gmail.com>
2017-12-19 18:21:54 +0100

    Difficult stuff with regexes

--- other/match.py
+++ other/match.py
@@ -9,6 +9,11 @@ pp = pprint.PrettyPrinter(indent=4)
 match = re.findall("\*(\w+)\*", sys.argv[1])
 pp.pprint( match )
 
-match = re.search("\*(\w+)\*", sys.argv[1])
-pp.pprint( match )
-print( match.group(1) )
+this_string = sys.argv[1]
+while match is not None:
+    match = re.search("\*(\w+)\*", this_string )
+    pp.pprint( match )
+    if match is not None:
+        print( match.group(1) )
+        this_string = this_string[match.start(1):]
+
