ccc999ef1279464413d28f1c75a27e5d4c11a62f
JJ Merelo <jjmerelo@gmail.com>
2017-12-27 10:07:37 +0100

    Starting with multiple dispatch, elixir style

--- other/multiple.exs
+++ other/multiple.exs
@@ -12,12 +12,22 @@ defimpl Stringifier, for: Span do
   def tostring(span), do: Enum.join(span.words," ")
 end
 
+defmodule Quoted do
+  @doc "A few words and a quote"
+  defstruct words: [], quote: "em"
+
+  def i(string, quote) do
+    "<#{quote}> #{string} </#{quote}>"
+  end
+end
 
 defmodule Main do
   def main do
     words = %Span{words: ["A","few","words"]}
     IO.puts(words.words)
     IO.puts(Stringifier.tostring(words))
+    quoted = %Quoted{words: ["More","words"], quote: "em"}
+    IO.puts(Quoted.i(quoted.words,quoted.quote))
   end
 end
 
